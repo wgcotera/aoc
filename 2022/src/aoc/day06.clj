f4fed93d2018063fa856224db0c24b3a254ff66d
wgcotera <wgcotera@espol.edu.ec>
2022-12-23 09:49:03 -0500

    day 06

---
+++ 2022/src/aoc/day06.clj
@@ -0,0 +1,26 @@
+(ns aoc.day06
+  (:require [aoc.util :as util]))
+
+(def input 
+  "Input data from file"
+  (str/split-lines (util/read-input "06" false)))
+
+(defn first-marker
+  "Returns the position of the first occurrence of a marker in a string"
+  [v-str n-digits]
+  (loop [s v-str n n-digits]
+    (if (= (count (distinct (take n-digits s))) n-digits)
+      n
+      (recur (drop 1 s) (inc n)))))
+
+(defn part-01
+  "characters need to be processed before the first start-of-packet marker taking 4"
+  []
+  (->> input
+       (map #(first-marker % 4))))
+
+(defn part-02
+  "characters need to be processed before the first start-of-packet marker taking 14"
+  []
+  (->> input
+       (map #(first-marker % 14))))
